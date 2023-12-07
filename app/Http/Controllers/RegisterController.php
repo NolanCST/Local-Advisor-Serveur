8c57614f35996cc20931bb18a1bcb14dec096b6d
Galuc <146081481+LucGaluc@users.noreply.github.com>
2023-12-07 14:21:25 +0100

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    protected function create(Request $request)
+    {
+        // Validation des données du formulaire
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'pseudo' => 'required',
+            'birthday' => 'required',
+            'status' => 'required',
+        ]);
+
+        // Création d'un nouvel utilisateur
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'pseudo' => $validatedData['pseudo'],
+            'birthday' => $validatedData['birthday'],
+            'status' => $validatedData['status'],
+        ]);
+
+        if ($user) {
+            // Utilisateur créé avec succès, retourne un JSON indiquant le succès
+            return response()->json(['message' => 'Utilisateur créé avec succès.'], 200);
+        } else {
+            // Utilisateur non créé, retourne un JSON avec un message d'erreur
+            return response()->json(['message' => 'Erreur lors de la création du compte. Veuillez réessayer.'], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Authentification
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+
+// Déconnexion
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Inscription
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'create'])->name('register');
